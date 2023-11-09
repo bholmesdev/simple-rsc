7da59d6d7d55484e9b484bf3c33036a8475c9048
bholmesdev <bholmesdev@gmail.com>
2023-11-09 12:58:30 -0500

    Revert "1 stream"
    This reverts commit 861c5014f97a5a8d6bf39be56769812bd6c03ee9.

--- app/page.jsx
+++ app/page.jsx
@@ -6,10 +6,10 @@ async function Albums() {
 	return (
 		<ul>
 			{albums.map((a) => (
-				<li key={a.id}>
-					<img src={a.cover} alt={a.title} />
+				<li key={a.id} className="flex gap-2 items-center mb-2">
+					<img className="w-20 aspect-square" src={a.cover} alt={a.title} />
 					<div>
-						<h3>{a.title}</h3>
+						<h3 className="text-xl">{a.title}</h3>
 						<p>{a.songs.length} songs</p>
 					</div>
 				</li>
@@ -21,7 +21,7 @@ async function Albums() {
 export default async function Page() {
 	return (
 		<>
-			<h1>Spotifn’t</h1>
+			<h1 className="text-3xl mb-3">Spotifn’t</h1>
 			<Suspense fallback="Getting albums">
 				<Albums />
 			</Suspense>
