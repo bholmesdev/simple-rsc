861c5014f97a5a8d6bf39be56769812bd6c03ee9
bholmesdev <bholmesdev@gmail.com>
2023-11-09 12:55:57 -0500

    1 stream

--- server.js
+++ server.js
@@ -4,27 +4,9 @@ import { Hono } from 'hono';
 import { build as esbuild } from 'esbuild';
 import * as ReactServerDom from 'react-server-dom-webpack/server.browser';
 import { createElement } from 'react';
-import { serveStatic } from '@hono/node-server/serve-static';
 
 const clientComponentMap = {};
 const app = new Hono();
-
-app.get('/', async (c) => {
-	const html = `
-<!DOCTYPE html>
-<html>
-<head>
-	<title>React Server Components from Scratch</title>
-	<script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body>
-	<div id="root"></div>
-	<script type="module" src="/build/_client.js"></script>
-</body>
-</html>`;
-
-	return c.html(html);
-});
 
 app.get('/rsc', async (c) => {
 	const { default: Page } = await import('./build/page.js');
@@ -32,8 +14,6 @@ app.get('/rsc', async (c) => {
 	const stream = ReactServerDom.renderToReadableStream(Comp, clientComponentMap);
 	return new Response(stream);
 });
-
-app.get('/build/*', serveStatic({ root: './' }));
 
 serve(app, async (info) => {
 	await build();
@@ -49,14 +29,6 @@ async function build() {
 		outdir: resolveBuild(),
 		// avoid bundling npm packages
 		packages: 'external'
-	});
-
-	await esbuild({
-		bundle: true,
-		format: 'esm',
-		logLevel: 'error',
-		entryPoints: [resolveApp('_client.jsx')],
-		outdir: resolveBuild()
 	});
 }
 
