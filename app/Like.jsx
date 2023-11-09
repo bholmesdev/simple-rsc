3a476026f8d891caa416bc04f3df11a28448cdab
bholmesdev <bholmesdev@gmail.com>
2023-11-09 12:41:06 -0500

    feat(app): add default styles

--- app/Like.jsx
+++ app/Like.jsx
@@ -4,5 +4,5 @@ import { useState } from 'react';
 
 export default function Like() {
 	const [likes, setLikes] = useState(0);
-	return <button onClick={() => setLikes(likes + 1)}>Likes {likes}</button>;
+	return <button onClick={() => setLikes(likes + 1)}>♥ {likes}</button>;
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,29 @@
 import { Suspense } from 'react';
-import { Albums } from './albums.jsx';
+import { getAll } from '../data/db.js';
 import Like from './Like.jsx';
+
+async function Albums() {
+	const albums = await getAll();
+	return (
+		<ul>
+			{albums.map((a) => (
+				<li key={a.id} className="flex gap-2 items-center mb-2">
+					<img className="w-20 aspect-square" src={a.cover} alt={a.title} />
+					<div>
+						<h3 className="text-xl">{a.title}</h3>
+						<p>{a.songs.length} songs</p>
+						<Like />
+					</div>
+				</li>
+			))}
+		</ul>
+	);
+}
 
 export default async function Page() {
 	return (
 		<>
-			<h1>From the server!</h1>
-			<Like />
+			<h1 className="text-3xl mb-3">Spotify clone</h1>
 			<Suspense fallback="Getting albums">
 				<Albums />
 			</Suspense>
