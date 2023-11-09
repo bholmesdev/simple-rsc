88730952324de1cfe9d41ea16a8b3c2394de5caa
bholmesdev <hey@bholmes.dev>
2023-11-09 17:35:39 -0500

    refactor: remove strictmode

--- app/_client.jsx
+++ app/_client.jsx
@@ -1,16 +1,16 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createFromFetch } from 'react-server-dom-webpack/client';
 
 // HACK: map webpack resolution to native ESM
 // @ts-expect-error Property '__webpack_require__' does not exist on type 'Window & typeof globalThis'.
 window.__webpack_require__ = async (id) => {
-	return import(id);
+  return import(id);
 };
 
 // @ts-expect-error
 const root = createRoot(document.getElementById('root'));
 
-createFromFetch(fetch(`/rsc`)).then((ele) => {
-	root.render(<StrictMode>{ele}</StrictMode>);
+createFromFetch(fetch('/rsc')).then(async (comp) => {
+  root.render(comp);
 });
+
