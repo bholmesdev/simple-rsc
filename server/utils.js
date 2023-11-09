ff2150a1f38d759791435a37028a2519df530ae8
bholmesdev <bholmesdev@gmail.com>
2023-11-09 12:08:58 -0500

    chore: remove unused utils

--- server/utils.js
+++ server/utils.js
@@ -1,4 +1,3 @@
-import fs from 'node:fs';
 import { fileURLToPath } from 'node:url';
 
 export const app = new URL('../app/', import.meta.url);
@@ -21,24 +20,3 @@ export function resolveApp(path) {
 export function resolveDist(path = '') {
 	return fileURLToPath(new URL(path, dist));
 }
-
-export function resolveClientDist(path) {
-	return new URL(path, resolveDist('client/'));
-}
-
-export function resolveServerDist(path) {
-	return new URL(path, resolveDist('server/'));
-}
-
-export const clientComponentMapUrl = resolveDist('clientComponentMap.json');
-
-export async function writeClientComponentMap(bundleMap) {
-	await fs.promises.writeFile(clientComponentMapUrl, JSON.stringify(bundleMap));
-}
-
-export async function readClientComponentMap() {
-	const bundleMap = await fs.promises.readFile(clientComponentMapUrl, 'utf-8');
-	return JSON.parse(bundleMap);
-}
-
-export const relativeOrAbsolutePathRegex = /^\.{0,2}\//;
